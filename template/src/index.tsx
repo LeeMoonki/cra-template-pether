ffc0d11191352210ddb10a18d663f5f3ffae24a2
Moonki Lee <shinbaek89@gmail.com>
2022-05-03 00:25:43 +0900

    apply react 18

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -6,19 +6,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-if (
-  !new (class {
-    x: any;
-  })().hasOwnProperty('x')
-) {
-  throw new Error('Transpiler is not configured correctly');
-}
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
