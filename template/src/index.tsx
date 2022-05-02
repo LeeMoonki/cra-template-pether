211cddc2c61ea275e57b6819fcb6e555e832663d
Moonki Lee <shinbaek89@gmail.com>
2022-05-03 00:34:59 +0900

    change react-dom to react-dom/client

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-prototype-builtins */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
